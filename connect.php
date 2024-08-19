b6544eea2a1b4fa557f82abddb10929e295bfd5d
antreaslamprou <146127103+antreaslamprou@users.noreply.github.com>
2024-08-19 22:56:40 +0300

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
 //Creates the connection to the database
-$dbhost = 'vesta.uclan.ac.uk';
-$dbuser = 'alambrou1';
-$dbpass = '4JHcmDPb';
-$dbname = 'alambrou1';
+$dbhost = 'host';
+$dbuser = 'user';
+$dbpass = 'password';
+$dbname = 'database';
 $connection = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 ?>
